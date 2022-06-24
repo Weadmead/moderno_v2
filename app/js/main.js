48a5a1628cdea5aaadd14722443b845ebb9ad181
Weadmead <wead758@gmail.com>
2022-06-24 05:49:48 +0300

    finish new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,34 @@
-$(function(){
-    
-    var mixer = mixitup('.products__inner-box');
+$(function () {
+  
+  // range-slider
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",    
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
+  //mixitup
+  var mixer = mixitup('.products__inner-box');
 
+  //star rating
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
 
-    $('.product-slider__inner').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      arrows: false,
-      dots: true
-    });  
+  //slider
+  $('.product-slider__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: true
+  });
+
 
 
 });
