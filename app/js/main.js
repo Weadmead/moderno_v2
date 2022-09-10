3ba0212867f1de3faa1c3b4fed8fcc714b402c38
Weadmead <wead758@gmail.com>
2022-09-10 04:04:27 +0300

    adaptive header + footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+  
+
   
 //sort by list
   $('.icon-th-list').on('click', function(){
@@ -43,8 +54,10 @@ $(function () {
   });
   
   $('.icon-th-list').on('click', function(){
-    $('.product__item').addClass('list')
+    $('.product__item').addClass('list');
   });
+
+
 
 
 
