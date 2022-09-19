46d7598bd8542fc96362bc8e00edeb787b196049
Weadmead <wead758@gmail.com>
2022-09-20 01:26:03 +0300

    added upload page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function () {
 
+
+  $('input[type="file"], select').styler();
+
   $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
     var id = $(this).attr('data-id');
       $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
